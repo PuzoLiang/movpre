28dae550fe9fac08c769c9413682c4c9ce4b2df7
Jing Malan <jing.l.malan@gmail.com>
2018-09-09 18:27:44 +0800

    add files

--- server/tpl/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  normal: require('./html'),
-  ejs:require('ejs')
-}
-
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './assets/common.scss'
+function changeTitle() {
+  window.$('#app').html('Parcel 打包包');
+}
+
+setTimeout(function(){
+  changeTitle()
+},2000);
