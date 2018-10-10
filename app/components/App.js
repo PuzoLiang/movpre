fcc3a564309a7e0b418484d51b3d9ea33fd5c067
wang zai <>
2018-10-10 15:02:50 +0800

    add component

---
+++ app/components/App.js
@@ -0,0 +1,17 @@
+import { React } from 'react';
+import { ReactDOM } from 'react-dom';
+
+class App extends ReactDOM.component {
+  constructor(){
+
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default App;
